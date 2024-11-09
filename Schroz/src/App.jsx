2ba5c505e38b97b5dc1f30efb59334e2f84c142a
kaungsett <rrocket438@gmail.com>
2024-11-09 19:01:45 +0630

    ch6 finished

--- Schroz/src/App.jsx
+++ Schroz/src/App.jsx
@@ -7,38 +7,43 @@ import { useApp } from "./ThemeApp";
 
 
 export default function App() {
- const { showForm } = useApp();
+ const { showForm  , setGlobalMsg} = useApp();
+
  const [data, setData] = useState([
  { id: 3, content: "Yay, interesting.", name: "Chris" },
  { id: 2, content: "React is fun.", name: "Bob" },
  { id: 1, content: "Hello, World!", name: "Alice" },
  ]);
+
  const remove = id => {
  setData(data.filter(item => item.id !== id));
+ setGlobalMssg("An item deleted");
  };
+
  const add = (content, name) => {
  const id = data[0].id + 1;
  setData([{ id, content, name }, ...data]);
+ setGlobalMssg("An item added");
  };
 
  return (
  <Box>
  <Header />
 
- <Container
- maxWidth="sm"
- sx={{ mt: 4 }}>
- {showForm && <Form add={add} />}
- {data.map(item => {
- return (
- <Item
- key={item.id}
- item={item}
- remove={remove}
- />
- );
- })}
- </Container>
+    <Container
+            maxWidth="sm"
+            sx={{ mt: 4 }}>
+        {showForm && <Form add={add} />}
+        {data.map(item => {
+    return (
+        <Item
+        key={item.id}
+        item={item}
+        remove={remove}
+        />
+    );
+    })}
+    </Container>
  </Box>
  );
 }
